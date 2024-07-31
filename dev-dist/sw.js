0ae5b5729dd61ac98e38d90968c1123ec9453431
Juan Cruz Cagnoni <juancagnoni@gmail.com>
2024-07-31 13:48:23 -0300

    sw

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ha4gmc43j1o"
+    "revision": "0.bkr4nkn5o4g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
